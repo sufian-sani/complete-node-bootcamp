40119d68c388fdacc5eee149fa684cada36c9bdb
abu-sani <abu.sani@byslglobal.com>
2024-07-14 18:58:54 +0600

    14-7 6:58

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const http = require('http')
 const url = require('url')
+const slugify = require('slugify')
+const replaceTemplate = require('./modules/replaceTemplate.js')
 
 ////////////////////////////////////////////////////////
 // Files
@@ -29,12 +31,6 @@ const url = require('url')
 ////////////////////////////////////////////////////////
 // Server
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-
-    return output
-}
-
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf8')
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf8')
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf8')
@@ -42,24 +38,41 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf8' )
 const dataObj = JSON.parse(data)
 
+// console.log(slugify('Fresh Avo',{lower: true, replacement:'-'}))
+
+const slugs = dataObj.map(el => slugify(el.productName, {lower: true}))
+// console.log(slugs)
+
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+    // console.log(req.url)
+    // const { query, pathname } = url.parse(req.url, true)
+    // console.log(query, pathname)
+
+    const { query, pathname } = url.parse(req.url, true)
+    // console.log(query.id, pathname)
+
+    // const pathname = req.url;
+    // console.log(pathname)
 
     // Overview page
-    if(pathName === '/' || pathName === '/overview') {
+    if(pathname === '/' || pathname === '/overview') {
         res.writeHead(200, {'Content-Type': 'text/html'})
 
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el))
-        console.log(cardsHtml)
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
 
-        res.end(tempOverview)
+        res.end(output)
 
     // Product page
-    } else if (pathName === '/product') {
-        res.end('This is the Product')
+    } else if (pathname === '/product') {
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        const product = dataObj[query.id]
+        // console.log(product)
+        const output = replaceTemplate(tempProduct, product)
+        res.end(output)
 
     // API
-    } else if (pathName === '/api') {
+    } else if (pathname === '/api') {
         res.writeHead(200, {'Content-Type': 'application/json'});
         res.end(data);
     } else {
