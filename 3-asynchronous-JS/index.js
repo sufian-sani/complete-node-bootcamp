32b2671e4f5c776c7818bc328b6745ebbefebe25
abu-sani <abu.sani@byslglobal.com>
2024-07-15 19:01:12 +0600

    15-7 7:01

---
+++ 3-asynchronous-JS/index.js
@@ -0,0 +1,14 @@
+const fs = require("fs")
+const superagent = require('superagent')
+
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+    console.log(`Breed: ${data}`)
+
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
+        console.log(res.body.message)
+
+        fs.writeFile('dog-imf.txt', res.body.message, err => {
+            console.log('Rondom file save')
+        })
+    })
+})
