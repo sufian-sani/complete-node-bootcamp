c0aea56ac10efd9297ccbd0e400bc0a385b664f6
abu-sani <abu.sani@byslglobal.com>
2024-07-28 15:34:31 +0600

    28-7 3:34

--- 4-natours/routers/tourRoutes.js
+++ 4-natours/routers/tourRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.param('id', tourController.checkID)
 
-router.route('/').get(tourController.getAllTours).post(tourController.createTour)
+router.route('/').get(tourController.getAllTours).post(tourController.checkBody ,tourController.createTour)
 router.route('/:id').delete(tourController.deleteTour).get(tourController.getTour).patch(tourController.updateTour)
 
 module.exports = router;
