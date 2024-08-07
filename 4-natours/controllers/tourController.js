55bea96295d011a6daf607845b91e36e9292621a
abu-sani <abu.sani@byslglobal.com>
2024-08-07 17:28:24 +0600

    7-8 5:28

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -6,9 +6,22 @@ exports.getAllTours = async (req, res) => {
         const queryObj = { ...req.query };
         const excludedFields = ['page','sort','limit','fields']
         excludedFields.forEach(el => delete queryObj[el]);
-        console.log(req.query, queryObj)
+        // console.log(req.query, queryObj)
 
-        const query = await Tour.find(queryObj)
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        console.log(JSON.parse(queryStr));
+
+        let query = Tour.find(JSON.parse(queryStr))
+
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ')
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt');
+        }
+
+        // console.log(req.query)
 
         const tours = await query;
 
