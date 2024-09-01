7ea5bbe4908f26a35460bb1b382acadfacb72e88
abu-sani <abu.sani@byslglobal.com>
2024-09-01 19:01:00 +0600

    1-9 7:00

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,36 +1,60 @@
 const Tour = require("../models/tourModel");
+const APIFeatures = require('./../utils/apiFeatures');
 
+exports.aliasTopTours = (req, res, next) => {
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+    next();
+};
 
 exports.getAllTours = async (req, res) => {
     try {
-        const queryObj = { ...req.query };
-        const excludedFields = ['page','sort','limit','fields']
-        excludedFields.forEach(el => delete queryObj[el]);
-        // console.log(req.query, queryObj)
-
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-        console.log(JSON.parse(queryStr));
-
-        let query = Tour.find(JSON.parse(queryStr))
-
-        if(req.query.sort){
-            const sortBy = req.query.sort.split(',').join(' ')
-            query = query.sort(sortBy);
-        } else {
-            query = query.sort('-createdAt');
-        }
-
-        if(req.query.fields){
-            const fields = req.query.fields.split(',').join(' ')
-            query = query.select(fields)
-        } else {
-            query = query.select('-__v')
-        }
-
-        // console.log(req.query)
-
-        const tours = await query;
+        // const queryObj = { ...req.query };
+        // const excludedFields = ['page','sort','limit','fields']
+        // excludedFields.forEach(el => delete queryObj[el]);
+        // // console.log(req.query, queryObj)
+        //
+        // let queryStr = JSON.stringify(queryObj);
+        // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        // console.log(JSON.parse(queryStr));
+        //
+        // let query = Tour.find(JSON.parse(queryStr))
+        //
+        // if(req.query.sort){
+        //     const sortBy = req.query.sort.split(',').join(' ')
+        //     query = query.sort(sortBy);
+        // } else {
+        //     query = query.sort('-createdAt');
+        // }
+        //
+        // if(req.query.fields){
+        //     const fields = req.query.fields.split(',').join(' ')
+        //     query = query.select(fields)
+        // } else {
+        //     query = query.select('-__v')
+        // }
+        //
+        // // pagination
+        // const page = req.query.page * 1 || 1;
+        // const limit = req.query.limit * 1 || 100;
+        // const skip = (page - 1) * limit;
+        // query = query.skip(skip).limit(limit);
+        //
+        // if(req.query.page){
+        //     const numTours = await Tour.countDocuments();
+        //     if(skip>=numTours) throw new Error('This page does not exist');
+        // }
+        //
+        // // console.log(req.query)
+        //
+        // const tours = await query;
+        const features = new APIFeatures(Tour.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
+        const tours = await features.query;
 
         res.status(200).json({
             status: 'success',
@@ -115,3 +139,42 @@ exports.deleteTour = async (req, res) => {
         })
     }
 }
+
+exports.getTourStats = async (req, res) => {
+    try {
+        const stats = await Tour.aggregate([
+            {
+                $match: { ratingsAverage: { $gte: 4.1 } }
+            },
+            {
+                $group: {
+                    _id: null,
+                    // numTours: { $sum: 1 },
+                    // numRatings: { $sum: '$ratingsQuantity' },
+                    avgRating: { $avg: '$ratingsAverage' },
+                    avgPrice: { $avg: '$price' },
+                    minPrice: { $min: '$price' },
+                    maxPrice: { $max: '$price' }
+                }
+            },
+            {
+                $sort: { avgPrice: 1 }
+            }
+            // {
+            //   $match: { _id: { $ne: 'EASY' } }
+            // }
+        ]);
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                stats
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
+};
--- 4-natours/routers/tourRoutes.js
+++ 4-natours/routers/tourRoutes.js
@@ -4,6 +4,12 @@ const router = express.Router();
 
 // router.param('id', tourController.checkID)
 
+router
+    .route('/top-5-cheap')
+    .get(tourController.aliasTopTours, tourController.getAllTours)
+
+router.route('/tour-stats').get(tourController.getTourStats);
+
 router.route('/').get(tourController.getAllTours).post(tourController.createTour)
 router.route('/:id').delete(tourController.deleteTour).get(tourController.getTour).patch(tourController.updateTour)
 
