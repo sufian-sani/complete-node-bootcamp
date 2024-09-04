54a2083999869f4deba25227377dca318aa4f451
abu-sani <abu.sani@byslglobal.com>
2024-09-04 16:49:14 +0600

    04-09 4:49

---
+++ 4-natours/app.js
@@ -0,0 +1,70 @@
+// const fs = require('fs');
+const express = require('express');
+const morgan = require('morgan')
+
+const tourRouter = require('./routers/tourRoutes');
+const userRouter = require('./routers/userRouters');
+
+const app = express();
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req,res,next)=>{
+    console.log('hello from the middleware')
+    next()
+})
+
+app.use((req,res,next)=>{
+    req.requestTime = new Date().toISOString();
+    next();
+})
+
+// const tours = JSON.parse(
+//     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf8')
+// )
+
+// app.get('/api/v1/tours', getAllTours)
+// app.get('/api/v1/tours/:id', getTour)
+// app.post('/api/v1/tours', createTour)
+// app.patch('/api/v1/tours/:id', updateTour)
+// app.delete('/api/v1/tours/:id', deleteTour)
+// const tourRouter = express.Router();
+// const userRouter = express.Router();
+
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
+
+app.all('*', (req, res, next) => {
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Can't find ${req.originalUrl} on this server!`
+    // });
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+    err.status = 'fail';
+    err.statusCode = 404;
+
+    next(err);
+})
+
+app.use((err, req, res, next)=>{
+    err.statusCode = err.statusCode || 500
+    err.status = err.status || 'error'
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+    })
+})
+
+// tourRouter.route('/').get(getAllTours).post(createTour)
+// tourRouter.route('/:id').delete(deleteTour).get(getTour).patch(updateTour)
+//
+// userRouter.route('/').get(getAllUsers).post(createUser)
+// userRouter.route('/:id').delete(deleteUser).get(getUser).patch(updateUser)
+
+module.exports = app;
+
