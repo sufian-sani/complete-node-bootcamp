ec06ae71d0ade265d01b84ababdbc69dbf3e1a45
abu-sani <abu.sani@byslglobal.com>
2024-09-05 14:21:29 +0600

    5-09 2:21

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,5 +1,6 @@
 const Tour = require("../models/tourModel");
 const APIFeatures = require('./../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
 
 exports.aliasTopTours = (req, res, next) => {
     req.query.limit = '5';
@@ -8,68 +9,22 @@ exports.aliasTopTours = (req, res, next) => {
     next();
 };
 
-exports.getAllTours = async (req, res) => {
-    try {
-        // const queryObj = { ...req.query };
-        // const excludedFields = ['page','sort','limit','fields']
-        // excludedFields.forEach(el => delete queryObj[el]);
-        // // console.log(req.query, queryObj)
-        //
-        // let queryStr = JSON.stringify(queryObj);
-        // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-        // console.log(JSON.parse(queryStr));
-        //
-        // let query = Tour.find(JSON.parse(queryStr))
-        //
-        // if(req.query.sort){
-        //     const sortBy = req.query.sort.split(',').join(' ')
-        //     query = query.sort(sortBy);
-        // } else {
-        //     query = query.sort('-createdAt');
-        // }
-        //
-        // if(req.query.fields){
-        //     const fields = req.query.fields.split(',').join(' ')
-        //     query = query.select(fields)
-        // } else {
-        //     query = query.select('-__v')
-        // }
-        //
-        // // pagination
-        // const page = req.query.page * 1 || 1;
-        // const limit = req.query.limit * 1 || 100;
-        // const skip = (page - 1) * limit;
-        // query = query.skip(skip).limit(limit);
-        //
-        // if(req.query.page){
-        //     const numTours = await Tour.countDocuments();
-        //     if(skip>=numTours) throw new Error('This page does not exist');
-        // }
-        //
-        // // console.log(req.query)
-        //
-        // const tours = await query;
-        const features = new APIFeatures(Tour.find(), req.query)
-            .filter()
-            .sort()
-            .limitFields()
-            .paginate();
-        const tours = await features.query;
+exports.getAllTours = catchAsync(async (req, res, next) => {
+    const features = new APIFeatures(Tour.find(), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate();
+    const tours = await features.query;
 
-        res.status(200).json({
-            status: 'success',
-            results: tours.length,
-            data:{
-                tours
-            }
-        })
-    } catch (err){
-        res.status(404).json({
-            status: 'fail',
-            message: err
-        })
-    }
-}
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data:{
+            tours
+        }
+    })
+})
 
 exports.getTour = async (req, res) => {
     try{
@@ -88,22 +43,33 @@ exports.getTour = async (req, res) => {
     }
 }
 
-exports.createTour = async (req, res) => {
-    try{
-        const newTour = await Tour.create(req.body)
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        })
-    } catch (err){
-        res.status(400).json({
-            status: 'fail',
-            message: 'Invalid data send!'
-        })
-    }
-}
+exports.createTour = catchAsync(async (req, res, next) => {
+    const newTour = await Tour.create(req.body)
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            tour: newTour
+        }
+    })
+})
+
+// exports.createTour = async (req, res) => {
+//     try{
+//         const newTour = await Tour.create(req.body)
+//         res.status(201).json({
+//             status: 'success',
+//             data: {
+//                 tour: newTour
+//             }
+//         })
+//     } catch (err){
+//         res.status(400).json({
+//             status: 'fail',
+//             message: 'Invalid data send!'
+//         })
+//     }
+// }
 
 exports.updateTour = async (req, res) => {
     try{
